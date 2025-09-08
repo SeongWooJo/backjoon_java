61717d2b80cd7acedf8e0da6bdec936d51e93efa
SeongWooJo <abc8325767@gmail.com>
2025-09-08 22:38:48 +0900

    BOJ-EX: 2025. 9. 8. 오후 10:38:46

--- 1929번 - 소수 구하기/Main.java
+++ 1929번 - 소수 구하기/Main.java
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                      :::    :::    :::     */
-/*   Problem Number: 4134                              :+:    :+:      :+:    */
+/*   Problem Number: 1929                              :+:    :+:      :+:    */
 /*                                                    +:+    +:+        +:+   */
 /*   By: abc8325767 <boj.kr/u/abc8325767>            +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/4134                           #+#        #+#      #+#    */
-/*   Solved: 2025/09/04 20:50:30 by abc8325767    ###          ###   ##.kr    */
+/*   https://boj.kr/1929                           #+#        #+#      #+#    */
+/*   Solved: 2025/09/08 19:34:16 by abc8325767    ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-				
-        int N = Integer.parseInt(br.readLine());
+		
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < N; i++) {
-            long input = Long.parseLong(br.readLine());
-            Loop:
-            for(long j = input; ; j++){
-                if(j == 0 || j == 1) continue;
-                if(j == 2 || j == 3){
-                    sb.append(j + "\n");
-                    break;
-                }
-                else
-                { 
-                    for(long k = 2; k <= Math.sqrt(j); k++){
-                        if(j % k == 0) continue Loop;
-                    }
-                    sb.append(j + "\n");
-                    break;
-                }
+        for(int i = M; i <= N; i++){
+            
+            if(check_decimal(i)) {
+                sb.append(i + "\n");
             }
         }
-        bw.write(sb.toString().trim());
+        
+        bw.write(sb.toString().trim() + "");
         bw.flush();
         bw.close();
+    }
+    static boolean check_decimal(int i){
+        if (i == 1) return false;
+        if ((i == 2) || (i == 3)) return true;
+        if ((i & 1) == 0) return false; // 짝수 컷
+        int limit = (int) Math.sqrt(i);
+        for(int j = 3; j <= limit; j+=2){
+            if(i % j == 0) return false;
+        }
+        return true;
     }
 }
