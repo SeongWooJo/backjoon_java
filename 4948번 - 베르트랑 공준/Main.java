0a407895cf54790c71bc928f3b12a3aedf3fa28a
SeongWooJo <abc8325767@gmail.com>
2025-09-12 00:29:11 +0900

    BOJ-EX: 2025. 9. 12. 오전 12:29:09

--- 4948번 - 베르트랑 공준/Main.java
+++ 4948번 - 베르트랑 공준/Main.java
@@ -1,14 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                      :::    :::    :::     */
-/*   Problem Number: 1929                              :+:    :+:      :+:    */
+/*   Problem Number: 4948                              :+:    :+:      :+:    */
 /*                                                    +:+    +:+        +:+   */
 /*   By: abc8325767 <boj.kr/u/abc8325767>            +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/1929                           #+#        #+#      #+#    */
-/*   Solved: 2025/09/08 19:34:16 by abc8325767    ###          ###   ##.kr    */
+/*   https://boj.kr/4948                           #+#        #+#      #+#    */
+/*   Solved: 2025/09/12 00:17:39 by abc8325767    ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 import java.io.*;
 import java.util.*;
@@ -18,29 +19,35 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        
         StringBuilder sb = new StringBuilder();
-        for(int i = M; i <= N; i++){
-            
-            if(check_decimal(i)) {
-                sb.append(i + "\n");
-            }
+        for(int input = -1;(input=Integer.parseInt(br.readLine()))!=0;){
+            int output = check_demical_num(input);
+            sb.append(output + "\n");
         }
-        
-        bw.write(sb.toString().trim() + "");
+
+        bw.write(sb.toString().trim());
         bw.flush();
         bw.close();
     }
-    static boolean check_decimal(int i){
-        if (i == 1) return false;
-        if ((i == 2) || (i == 3)) return true;
-        if ((i & 1) == 0) return false;
-        int limit = (int) Math.sqrt(i);
-        for(int j = 3; j <= limit; j+=2){
-            if(i % j == 0) return false;
+
+    static int check_demical_num(int N) {
+        int result_num = 0;
+
+        for(int i = N + 1; i <= 2*N; i++){
+            if(check_demical(i)) result_num += 1;
+        }
+        return result_num;
+    }
+
+    static boolean check_demical(int num){
+        if(num == 1) return false;
+        if((num % 2 == 0) || (num % 3 == 0) || (num % 5 == 0) || (num % 7 == 0)){
+            if((num == 2) || (num == 3) || (num == 5) || (num == 7)) return true;
+            else return false;
+        }
+        int limit = (int) Math.sqrt(num);
+        for(int i = 10; i <= limit; i++){
+            if(num % i == 0) return false;
         }
         return true;
     }
